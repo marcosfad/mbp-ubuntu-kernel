11fe26b0b6a1a49ef8da88d76f8e09a4b3e2473d
marcosfad <turkos+data@gmail.com>
2021-08-29 19:40:13 +0200

    Fix patches in HWE

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -27,7 +27,7 @@ docker run \
       --patchset1Commit=9511d5ed2ae0e851dd6a82843daefb2be7d5e212 \
       --patchset1Filter=\"grep -vE '000[0-9]'\" \
       --releaseSuffix=t2-aunali1 \
-      --releasePath=/repo/.build/t2-aunali1 \
+      --releasePath=/repo/build/t2-aunali1 \
 "
 docker run \
   -t \
@@ -50,7 +50,7 @@ docker run \
       --patchset2Commit=843ecfcaaec0a10707d447ac6d1840db940a9d29 \
       --patchset2Filter=\"grep -E '8001'\" \
       --releaseSuffix=t2-aunali1-patched \
-      --releasePath=/repo/.build/t2-aunali1-patched \
+      --releasePath=/repo/build/t2-aunali1-patched \
 "
 # jamlam
 VERSION=$(ubuntu-mainline-kernel.sh -r v5.13 | grep v | rev | cut -d'v' -f 1 | rev | tail -1)
@@ -71,7 +71,7 @@ docker run \
       --patchset1Commit=843ecfcaaec0a10707d447ac6d1840db940a9d29 \
       --patchset1Filter=\"grep -vE '0001|800[0-9]'\" \
       --releaseSuffix=t2-jamlam \
-      --releasePath=/repo/.build/t2-jamlam \
+      --releasePath=/repo/build/t2-jamlam \
 "
 
 docker run \
@@ -94,7 +94,8 @@ docker run \
       --patchset2Branch=master \
       --patchset2Commit=9511d5ed2ae0e851dd6a82843daefb2be7d5e212 \
       --patchset2Filter=\"grep -E 'brcmfmac'\" \
-      --releasePath=/repo/.build/t2-jamlam-patched \
+      --releaseSuffix=t2-jamlam-patched
+      --releasePath=/repo/build/t2-jamlam-patched \
 "
 
 # HWE
@@ -107,14 +108,34 @@ docker run \
     cd /repo \
     && \
     ./build.sh \
-      --kernel=${{ steps.variables.outputs.version }} \
-      --kernelBranch=hwe-5.11 \
+      --kernel=5.11.0 \
+      --kernelBranch=Ubuntu-hwe-5.11-5.11.0-34.36_20.04.1 \
       --kernelRepository='git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/focal' \
       --debianSaucePatches=no \
       --patchset1Repo='git://github.com/AdityaGarg8/5.10-patches.git' \
       --patchset1Branch=main \
-      --patchset1Commit=1544718ff8282e75bbade89faebcb3ac45e5434a \
-      --patchset1Filter=\"grep -vE '000[0-9]'\" \
+      --patchset1Commit=e509d58d4127ecd9c17292560f10fc089f1ad030 \
+      --patchset1Filter=\"grep -vE '0001'\" \
       --releaseSuffix=t2-hwe \
-      --releasePath=/repo/.build/t2-hwe \
+      --releasePath=/repo/build/t2-hwe \
+"
+docker run \
+  -t \
+  --rm \
+  -v "$(pwd)":/repo \
+  ${DOCKER_IMAGE} \
+  /bin/bash -c "\
+    cd /repo \
+    && \
+    ./build.sh \
+      --kernel=5.13.0 \
+      --kernelBranch=Ubuntu-hwe-5.13-5.13.0-14.14_20.04.2 \
+      --kernelRepository='git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/focal' \
+      --debianSaucePatches=no \
+      --patchset1Repo=git://github.com/jamlam/mbp-16.1-linux-wifi.git \
+      --patchset1Branch=main \
+      --patchset1Commit=843ecfcaaec0a10707d447ac6d1840db940a9d29 \
+      --patchset1Filter=\"grep -vE '0001'\" \
+      --releaseSuffix=t2-hwe-next \
+      --releasePath=/repo/build/t2-hwe-next \
 "
